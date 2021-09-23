ff87c353496349325b51f33717fdacd4293af266
Francisco <francisco.marquez.solt@gmail.com>
2021-09-23 13:44:34 -0500

    Linter, extensiones y deploy con Now

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Start")
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const container = document.getElementById('app')
+
+ReactDOM.render(<h1>"Hola Platzi!"</h1>, container)
