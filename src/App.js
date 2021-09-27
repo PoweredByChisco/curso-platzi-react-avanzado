d8cc4c061b4c39480c4f0f4a61ebe0d05b1e327f
Francisco <francisco.marquez.solt@gmail.com>
2021-09-27 17:04:26 -0500

    Creando PhotoCard y usando react-icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+/* eslint-disable react/jsx-closing-tag-location */
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './globalStyle'
+import { ListOfPhotoCards } from './components/ListOfPhotoCars'
 
 export const App =
- () => (<><GlobalStyle /> <ListOfCategories /></>)
+ () => (<>
+   <GlobalStyle />
+   <ListOfCategories />
+   <ListOfPhotoCards />
+ </>)
