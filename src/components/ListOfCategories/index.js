2ad435be2d72b17d7fd4b290cf31069cae4b80fe
Francisco <francisco.marquez.solt@gmail.com>
2021-09-28 13:57:13 -0500

    ¿Qué son los Hooks?

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import mock from '../../mock'
 
 export const ListOfCategories = () => {
-  const { categories } = mock
-  console.log(categories)
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await window.fetch(
+        'https://petgram-server-fco-o0ywu3gts-poweredbychisco.vercel.app/categories'
+      )
+      const data = await response.json()
+      setCategories(data)
+    }
+
+    fetchCategories()
+  }, []) /* The second parameter is for that the hook run when the component mount  */
+
   return (
     <List>
       {categories.map(item => <Item key={item.id}><Category {...item} /></Item>)}
