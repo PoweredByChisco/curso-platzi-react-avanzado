aaa8611180ecb550e9a9ad6b1edfcd77fbd89b84
Francisco <francisco.marquez.solt@gmail.com>
2021-09-27 20:05:09 -0500

    Commit

--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,4 +1,23 @@
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
+
+
+const fadeInKeyFrames = keyframes`
+  from {
+    filter: blur(5px);
+    opacity: 0;
+  }
+  
+  to {
+    filter: blur(0);
+    opacity: 1;
+  }
+  `
+
+const fadeIn = ({ time = '1s', type = 'ease' } = {}) => {
+  css`
+    animation: 1s ${fadeInKeyFrames} ease;
+  `
+}
 
 export const ImgWrapper = styled.div`
   border-radius: 10px;
