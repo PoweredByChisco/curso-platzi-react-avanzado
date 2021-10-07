acac38699cb4f33526a9b6de37a4ca653fcd8911
Francisco <francisco.marquez.solt@gmail.com>
2021-10-07 13:07:18 -0500

    Rutas protegidas

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import { Home } from './pages/Home'
 import { Router } from '@reach/router'
 import { Detail } from './pages/Detail'
 import { NavBar } from './components/NavBar'
+import { Favs } from './pages/Favs'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { User } from './pages/User'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App =
   () => {
@@ -18,6 +25,20 @@ export const App =
           <Home path='/pet/:id' />
           <Detail path='/detail/:detailId' />
         </Router>
+        <UserLogged>
+          {
+            ({ isAuth }) =>
+              isAuth
+                ? <Router>
+                  <Favs path='/favs' />
+                  <User path='/user' />
+                </Router>
+                : <Router>
+                  <NotRegisteredUser path='/favs' />
+                  <NotRegisteredUser path='/user' />
+                </Router>
+          }
+        </UserLogged>
         <NavBar />
       </>
     )
