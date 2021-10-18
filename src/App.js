865de1be50ba331b2a50b2e0b61a3fba8fe645e7
Francisco <francisco.marquez.solt@gmail.com>
2021-10-18 14:27:46 -0500

    Últimos retoques a las rutas de nuestra aplicación

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
 /* eslint-disable react/jsx-closing-tag-location */
-import React from 'react'
+import React, { useContext } from 'react'
 import { GlobalStyle } from './styles/globalStyle'
 import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
-import { Router } from '@reach/router'
+import { Router, Redirect } from '@reach/router'
 import { Detail } from './pages/Detail'
 import { NavBar } from './components/NavBar'
 import { Favs } from './pages/Favs'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { User } from './pages/User'
-import Context from './Context'
+import { Context } from './Context'
+import { NotFound } from './pages/NotFound'
 
 export const App =
   () => {
+    const { isAuth } = useContext(Context)
     return (
       <>
         <GlobalStyle />
         <Logo />
         <Router>
+          <NotFound default />
           <Home path='/' />
           <Home path='/pet/:id' />
           <Detail path='/detail/:detailId' />
+          {!isAuth && <NotRegisteredUser path='/login' />}
+          {!isAuth && <Redirect from='/favs' to='/login' />}
+          {!isAuth && <Redirect from='/user' to='/login' />}
+          {isAuth && <Redirect from='/login' to='/' />}
+          <Favs path='/favs' />
+          <User path='/user' />
         </Router>
-        <Context.Consumer>
-          {
-            ({ isAuth }) =>
-              isAuth
-                ? <Router>
-                  <Favs path='/favs' />
-                  <User path='/user' />
-                </Router>
-                : <Router>
-                  <NotRegisteredUser path='/favs' />
-                  <NotRegisteredUser path='/user' />
-                </Router>
-          }
-        </Context.Consumer>
         <NavBar />
       </>
     )
